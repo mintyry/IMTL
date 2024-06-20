bfe84a51bd532840793797e174edc9fb6a6d3656
mintyry <gitmintyry@gmail.com>
2024-06-19 21:44:47 -0700

    saved

--- server/server.js
+++ server/server.js
@@ -1,17 +1,28 @@
+// import necessary pckgs
 const express = require('express');
 const mysql = require('mysql2');
+// .congif() loads env file into process environment
+require('dotenv').config();
+
+// initialize express app
 const app = express();
 const port = 3000;
 
-// middleware to parse JSON bodies
+// use necessary middleware
+// parse JSON bodies
 app.use(express.json());
 
 // database connection
 const db = mysql.createConnection(
     {
-        host: 'localhost',
-        user: 'username',
-        password: 'password',,
-        database: 'dbName'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     }
-)
+);
+
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_NAME);

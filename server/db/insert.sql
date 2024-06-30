c221e33833ccfdfe62b28a1f85ab2af879b1dbb4
mintyry <gitmintyry@gmail.com>
2024-06-29 20:50:36 -0700

    made table with insert

---
+++ server/db/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO pr (id, name, color)
+VALUES
+    (01, 'Jason Lee Scott', 'red'),
+    (02, 'Zack Taylor', 'black'),
+    (03, 'Billy Cranston', 'blue'),
+    (04, 'Trini Kwan', 'yellow'),
+    (05, 'Kimberly Hart', 'pink');
+
+SELECT * FROM pr;
--- server/db/schema.sql
+++ server/db/schema.sql
@@ -3,3 +3,10 @@ DROP DATABASE IF EXISTS imtl_db;
 CREATE DATABASE imtl_db;
 
 USE imtl_db;
+
+-- dont leave trailing comma; produces error
+CREATE TABLE pr(
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    color VARCHAR(15) NOT NULL
+);

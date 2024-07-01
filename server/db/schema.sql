7aa26ef2ae37af3c6dc39f5d7d33bd15638dd6b6
mintyry <gitmintyry@gmail.com>
2024-06-30 19:49:13 -0700

    added more col to pr table

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -6,7 +6,11 @@ USE imtl_db;
 
 -- dont leave trailing comma; produces error
 CREATE TABLE pr(
-    id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
-    color VARCHAR(15) NOT NULL
+    color VARCHAR(15) NOT NULL,
+    year YEAR NOT NULL,
+    power VARCHAR(30),
+    season INT NOT NULL,
+    series_name VARCHAR(30)
 );

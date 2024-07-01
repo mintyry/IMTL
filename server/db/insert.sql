f385ee80620697810b43a8ebd85cf9801cb26835
mintyry <gitmintyry@gmail.com>
2024-06-30 20:11:00 -0700

    added tommy-white to table, added image column

--- server/db/insert.sql
+++ server/db/insert.sql
@@ -1,9 +1,20 @@
-INSERT INTO pr (id, name, color)
+INSERT INTO mmpr (
+    id,
+    name,
+    color,
+    year,
+    power,
+    season,
+    series_name,
+    image
+    )
 VALUES
-    (01, 'Jason Lee Scott', 'red'),
-    (02, 'Zack Taylor', 'black'),
-    (03, 'Billy Cranston', 'blue'),
-    (04, 'Trini Kwan', 'yellow'),
-    (05, 'Kimberly Hart', 'pink');
+    (01, 'Jason Lee Scott', 'red', 1993, 'tyrannosaurus', 1, 'mighty morphin'' power rangers'),
+    (02, 'Zack Taylor', 'black', 1993, 'mastodon', 1, 'mighty morphin'' power rangers'),
+    (03, 'Billy Cranston', 'blue', 1993, 'triceratops', 1, 'mighty morphin'' power rangers'),
+    (04, 'Trini Kwan', 'yellow', 1993, 'saber-toothed tiger', 1, 'mighty morphin'' power rangers'),
+    (05, 'Kimberly Hart', 'pink', 1993, 'pterodactyl', 1, 'mighty morphin'' power rangers'),
+    (06, 'Tommy Oliver', 'green', 1993, 'dragonzord', 1, 'mighty morphin'' power rangers');
+    (07, 'Tommy Oliver', 'white', 1994, 'tigerzord', 2, 'mighty morphin'' power rangers');
 
-SELECT * FROM pr;
+SELECT * FROM mmpr;
--- server/db/schema.sql
+++ server/db/schema.sql
@@ -5,12 +5,13 @@ CREATE DATABASE imtl_db;
 USE imtl_db;
 
 -- dont leave trailing comma; produces error
-CREATE TABLE pr(
+CREATE TABLE mmpr(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     color VARCHAR(15) NOT NULL,
     year YEAR NOT NULL,
     power VARCHAR(30),
     season INT NOT NULL,
-    series_name VARCHAR(30)
+    series_name VARCHAR(30),
+    image VARCHAR(50)
 );

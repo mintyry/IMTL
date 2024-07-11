b65d8cfa4846d4b2109935bed53ca2e1e0af9509
mintyry <gitmintyry@gmail.com>
2024-07-10 20:20:16 -0700

    change mmpr color values to json arrays

--- server/db/insert.sql
+++ server/db/insert.sql
@@ -9,12 +9,12 @@ INSERT INTO mmpr (
     image
     )
 VALUES
-    (01, 'Jason Lee Scott', 'red', 1993, 'tyrannosaurus', '[1,2]', 'mighty morphin'' power rangers', NULL),
-    (02, 'Zack Taylor', 'black', 1993, 'mastodon', '[1,2]', 'mighty morphin'' power rangers', NULL),
-    (03, 'Billy Cranston', 'blue', 1993, 'triceratops', '[1,2]', 'mighty morphin'' power rangers', NULL),
-    (04, 'Trini Kwan', 'yellow', 1993, 'saber-toothed tiger', '[1,2]', 'mighty morphin'' power rangers', NULL),
-    (05, 'Kimberly Hart', 'pink', 1993, 'pterodactyl', '[1,2]', 'mighty morphin'' power rangers', NULL),
-    (06, 'Tommy Oliver', 'green', 1993, 'dragonzord', '[1,2]', 'mighty morphin'' power rangers', NULL);
+    (01, 'Jason Lee Scott', '["red"]', 1993, 'tyrannosaurus', '[1,2]', 'mighty morphin'' power rangers', '[1,2]'),
+    (02, 'Zack Taylor', '["black"]', 1993, 'mastodon', '[1,2]', 'mighty morphin'' power rangers', '[1,2]'),
+    (03, 'Billy Cranston', '["blue"]', 1993, 'triceratops', '[1,2]', 'mighty morphin'' power rangers', '[1,2,3]'),
+    (04, 'Trini Kwan', '["yellow"]', 1993, 'saber-toothed tiger', '[1,2]', 'mighty morphin'' power rangers', '[1,2]'),
+    (05, 'Kimberly Hart', '["pink"]', 1993, 'pterodactyl', '[1,2]', 'mighty morphin'' power rangers', '[1,2,3]'),
+    (06, 'Tommy Oliver', '["green", "white"]', 1993, 'dragonzord', '[1,2]', 'mighty morphin'' power rangers', '[1,2,3]');
 
 SELECT * FROM mmpr;
 
